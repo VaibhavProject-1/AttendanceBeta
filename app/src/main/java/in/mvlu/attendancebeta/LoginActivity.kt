1865c557ec71ab433feb3d69b427069a3b177d21
VaibhavProject-1 <vaibhavpatel003@gmail.com>
2023-02-28 02:09:17 +0530

    Initially pushing just the layout and activities

---
+++ app/src/main/java/in/mvlu/attendancebeta/LoginActivity.kt
@@ -0,0 +1,25 @@
+package `in`.mvlu.attendancebeta
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import `in`.mvlu.attendancebeta.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnLogin.setOnClickListener {
+            startActivity(Intent(this, HomeActivity::class.java))
+        }
+        binding.tvHaventAccount.setOnClickListener{
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+
+    }
+}
